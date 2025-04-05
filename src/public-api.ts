3bab932d1be3fbc9f5ec8cc38f6238ead9f7b072
Paul Glazkow <gpb@hos247.com>
2025-04-05 07:40:24 +0400

    add plugin discovery decotator to public-api.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,5 +1,6 @@
 export * from './types';
 export * from './plugin-discovery/plugin-discovery.base';
+export * from './plugin-discovery/plugin-discovery-with-derivatives.decorator';
 export * from './plugin/plugin.base';
 export * from './plugin-manager/plugin-manager.base';
 export * from './plugin-builder/plugin-builder.base';
